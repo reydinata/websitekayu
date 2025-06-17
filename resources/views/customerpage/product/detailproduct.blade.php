536ee110f91ee7ca9792caedf87fa5634b5467d5
reydinata <125807167+reydinata@users.noreply.github.com>
2025-06-17 19:35:28 +0700

    Update detailproduct.blade.php

--- resources/views/customerpage/product/detailproduct.blade.php
+++ resources/views/customerpage/product/detailproduct.blade.php
@@ -201,7 +201,7 @@
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
           <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">Dewi</span>
+            <span class="sitename">Woodland</span>
           </a>
           <div class="footer-contact pt-3">
             <p>A108 Adam Street</p>
