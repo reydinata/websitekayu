2b93aef5d10bd69e5eaf7fe93525b455b89f7f14
reydinata <125807167+reydinata@users.noreply.github.com>
2025-06-17 19:36:13 +0700

    Update daftarpembelian.blade.php

--- resources/views/customerpage/product/daftarpembelian.blade.php
+++ resources/views/customerpage/product/daftarpembelian.blade.php
@@ -305,7 +305,7 @@ function showPaymentInfo(select) {
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
           <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">Dewi</span>
+            <span class="sitename">Woodland</span>
           </a>
           <div class="footer-contact pt-3">
             <p>A108 Adam Street</p>
@@ -358,7 +358,7 @@ function showPaymentInfo(select) {
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Dewi</strong> <span>All Rights Reserved</span></p>
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">Woodland</strong> <span>All Rights Reserved</span></p>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you've purchased the pro version. -->
